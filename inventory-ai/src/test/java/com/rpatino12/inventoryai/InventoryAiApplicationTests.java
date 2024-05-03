0bee9494ba9295e0ae7a67ef49db9335451eb90e
Ricardo Patino <rpatino12@outlook.com>
2024-05-03 12:16:20 -0500

    Add the spring initializr project files

---
+++ inventory-ai/src/test/java/com/rpatino12/inventoryai/InventoryAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpatino12.inventoryai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
